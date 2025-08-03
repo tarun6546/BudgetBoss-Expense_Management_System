ce3949a7e8a994119e314feeadce6e572fac18ad
tarun6546 <tarunvarshney2112@gmail.com>
2025-08-03 21:20:04 +0530

    khgu

--- server/server.js
+++ server/server.js
@@ -45,7 +45,9 @@ const app = express();
 // };
 
 
-app.use(cors("*"));
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(morgan('dev'));
