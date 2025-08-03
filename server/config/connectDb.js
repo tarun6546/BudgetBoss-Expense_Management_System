df81a91065a011992a29fe86049646bd93239a63
tarun6546 <tarunvarshney2112@gmail.com>
2025-08-03 12:33:12 +0530

    feat: Initialize server and implement user, transaction, and budget routes
    - Created package.json for server dependencies and scripts.
    - Implemented budget routes for adding, retrieving, updating, and deleting budgets.
    - Implemented transaction routes for adding, editing, deleting, and retrieving transactions.
    - Implemented user routes for login, registration, and updating user profiles.
    - Set up server with Express, CORS, Morgan, and dotenv for environment variable management.
    - Established database connection and configured middleware for JSON parsing and logging.
    - Added a welcome route for the expense management system.

--- server/config/connectDb.js
+++ server/config/connectDb.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const colors = require('colors');
+require('dotenv').config();
 // const { default: mongoose } = require('mongoose');
 
 const connectDb = async () => {
     try {
         console.log(process.env.MONGODB_URL)
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect("mongodb+srv://tarunvarshney2112:Tarun21122003@cluster0.sz0k6ac.mongodb.net/expanseApp?retryWrites=true&w=majority&appName=Cluster0");
         console.log(`Server running on ${mongoose.connection.host}`.bgBlue.white);
     }
     catch (error) {
